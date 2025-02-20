a49d0a00bd830a51977bdff2f8a1bdb4578b151f
Ubuntu <ubuntu@ip-172-31-2-67.ap-south-1.compute.internal>
2025-02-20 14:27:01 +0000

    Terraform Metaargument

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-terraform {
+ terraform {
 required_providers {
 	aws = {
 	source = "hashicorp/aws"
@@ -10,4 +10,21 @@ required_providers {
 
 provider "aws" {
 region = "ap-south-1"
+}
+
+locals {
+	instances = {"instance-A":"ami-0ddfba243cbee3768","instance-B":"ami-0ddfba243cbee3768",
+	"Instance-C":"ami-00bb6a80f01f03502","Instance-D":"ami-00bb6a80f01f03502"}
+}
+
+resource "aws_instance" "aws_ec2_instance" {
+
+for_each = local.instances
+ami = each.value
+instance_type = "t2.micro"
+
+tags = {
+Name = each.key
+
+}
 }
